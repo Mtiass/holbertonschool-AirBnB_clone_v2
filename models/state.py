98836c8fd32b16feb684ca80c1af79cbceb57b8e
Mtiass <MDLSproff@gmail.com>
2024-03-22 15:12:49 -0300

    state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    state_id = Column(
+        String(60),
+        ForeignKey('states.id'),
+        nullable=False
+    )
+
+    cities = relationship(
+        "City",
+        backref="state",
+        cascade="all, delete",
+        passive_deletes=True
+    )
+
+    @property
+    def cities(self):
+        """Getter for cities in FileStorage"""
+        acities = self.cities
+        filtercit = [city for city in acities if city.state_id == self.id]
+        return filtercit
