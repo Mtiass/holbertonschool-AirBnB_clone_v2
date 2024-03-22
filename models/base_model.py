b47092f9ad367b7769c0a920759930f97ddaebfa
German1127 <kleverk2023@gmail.com>
2024-03-22 10:26:42 -0700

    update __init__base

--- models/base_model.py
+++ models/base_model.py
@@ -48,6 +48,8 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
+                
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
