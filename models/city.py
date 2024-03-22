96af5989addb4df4a5eeb47e819fc47c0720bffa
Mtiass <MDLSproff@gmail.com>
2024-03-22 16:56:56 -0300

    city update

--- models/city.py
+++ models/city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
@@ -20,4 +19,4 @@ class City(BaseModel, Base):
         nullable=False
         )
     places = relationship(
-        "Place", backref="cities")
+        "Place", back_populates="cities", cascade="all, delete")
