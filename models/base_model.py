6738db6018b79b77735f64cef4e16850a6494c23
German1127 <kleverk2023@gmail.com>
2024-03-22 10:03:57 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,28 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, DateTime, String
 
+Base = declarative_base()
 
 class BaseModel:
+    
+    id = Column(
+        String(60),
+        nullable=False,
+        primary_key=True
+    )
+    created_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow()
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow()
+    )
+    
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
