40c1e8635d2a8e4b027f869e9924f7d4fe32e90c
German1127 <kleverk2023@gmail.com>
2024-03-23 08:07:17 -0700

    fix place

--- models/place.py
+++ models/place.py
@@ -8,11 +8,15 @@ from sqlalchemy.orm import relationship
 import os
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column('place_id',
+                             String(60),
                              ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id',
+                             String(60),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False))
 
