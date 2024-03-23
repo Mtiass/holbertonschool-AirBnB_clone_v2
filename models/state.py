e50f24e7dbef9cd88ca845027803a4edc0c47c6f
German1127 <kleverk2023@gmail.com>
2024-03-23 13:50:01 -0700

    update state

--- models/state.py
+++ models/state.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" class State  """
+from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """ class State """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="states")
+    else:
+        name = ""
 
-    name = Column(
-        String(128),
-        nullable=False
-    )
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            city_list = []
+            all_city = storage.all(City)
+            for _, v in all_city.items():
+                if self.id == v.state_id:
+                    city_list.append(v)
+            return city_list
 
-    state_id = Column(
-        String(60),
-        ForeignKey('states.id'),
-        nullable=False
-    )
-
-    cities = relationship(
-        "City",
-        backref="state",
-        cascade="all, delete",
-        passive_deletes=True
-    )
-
-    @property
-    def cities(self):
-        """Getter for cities in FileStorage"""
-        acities = self.cities
-        filtercit = [city for city in acities if city.state_id == self.id]
-        return filtercit
