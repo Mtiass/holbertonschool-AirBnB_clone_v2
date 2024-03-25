9d1eb1d98a0591fde283d729f1c4a2731ddb8bde
German1127 <kleverk2023@gmail.com>
2024-03-25 12:52:52 -0700

    update city

--- models/city.py
+++ models/city.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
 from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-
+    __tablename__ = 'cities'
     name = Column(
-        String(128),
-        nullable=False
-        )
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     state_id = Column(
-        String(60),
-        ForeignKey('states.id'),
-        nullable=False
-        )
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     places = relationship(
-        "Place", backref="cities")
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+
+# cascade: En SQLAlchemy, se utiliza para especificar cómo se propagan las operaciones de cambio entre objetos relacionados
+# backref: En SQLAlchemy, se utiliza para establecer automáticamente una relación inversa entre dos clases
