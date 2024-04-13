c115391cea89156e2bf316cdbe2474d63a3ac7ca
Mtiass <MDLSproff@gmail.com>
2024-04-13 15:15:02 -0300

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+
+wapp = Flask(__name__)
+
+
+@wapp.route('/', strict_slashes=False)
+def greetings():
+    return "Hello HBNB!"
+
+@wapp.route('/hbnb', strict_slashes=False)
+def greetings():
+    return "HBNB"
+
+if __name__ == "__main__":
+    wapp.run(host='0.0.0.0', port=5000)
